8ab33ad12029c044068ebecb7e238cdd3cc20fa1
Guy Royse <guy@guyroyse.com>
2015-07-10 16:02:10 -0400

    changes for code club on commited date

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,18 +1,27 @@
 class VendingMachine
-  VALUES = {:nickel => 0.05, :dime => 0.10 , :quarter => 0.25}
+
+  attr_reader :coin_return
+
+  VALUES = {
+    :nickel => 5,
+    :dime => 10,
+    :quarter => 25
+  }
 
   def initialize
     @sum = 0
-    @displayText = 'INSERT COIN'
+    @coin_return = []
   end
 
   def display
-    @displayText
+    @sum == 0 ? 'INSERT COIN' : "%0.2f" % (@sum / 100.0)
   end
 
   def insertCoin(coin)
-    @sum += VALUES[coin]
-    @displayText = "%0.2f" % @sum
+    if VALUES.has_key?(coin)
+      @sum += VALUES[coin]
+    else
+      @coin_return << coin
+    end
   end
-
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,22 +1,76 @@
 describe VendingMachine do
 
-  it 'displays INSERT COIN when no coins are inserted' do
-    expect(subject.display).to eq 'INSERT COIN'
+  context 'when no coins have been inserted' do
+
+    it 'displays INSERT COIN' do
+      expect(subject.display).to eq 'INSERT COIN'
+    end
+
+    it 'has an empty coin return' do
+      expect(subject.coin_return).to be_empty
+    end
+
   end
 
-  it 'displays 5 cents when a nickel inserted' do
-    subject.insertCoin(:nickel)
-    expect(subject.display).to eq '0.05'
+  context 'when a nickel has been inserted' do
+
+    before :each do
+      subject.insertCoin(:nickel)
+    end
+
+    it 'has an empty coin return' do
+      expect(subject.coin_return).to be_empty
+    end
+
+    it 'displays 5 cents' do
+      expect(subject.display).to eq '0.05'
+    end
+
   end
 
-  it 'displays 15 cents when a nickel and dime are inserted' do
+  context 'when a dime has been inserted' do
+
+    before :each do
+      subject.insertCoin(:dime)
+    end
+
+    it 'displays 10 cents' do
+      expect(subject.display).to eq '0.10'
+    end
+
+  end
+
+  context 'when a quarter has been inserted' do
+
+    before :each do
+      subject.insertCoin(:quarter)
+    end
+
+    it 'displays 25 cents' do
+      expect(subject.display).to eq '0.25'
+    end
+
+  end
+
+  it 'adds multiple coins together' do
     subject.insertCoin(:nickel)
     subject.insertCoin(:dime)
-    expect(subject.display).to eq '0.15'
+    subject.insertCoin(:quarter)
+    expect(subject.display).to eq '0.40'
   end
 
-  it 'displays 25 cents when a quarter is inserted' do
-    subject.insertCoin(:quarter)
-    expect(subject.display).to eq '0.25'
+  context 'when an invalid coin is inserted' do
+    it 'sends invalid coin to return and display remains unchanged' do
+      expect { subject.insertCoin(:penny) }.not_to change { subject.display }
+      expect(subject.coin_return).to contain_exactly(:penny)
+    end
+
+    it 'sends additional invalid coin to coin return and display remains unchanged' do
+      expect do
+        subject.insertCoin(:penny)
+        subject.insertCoin(:peso)
+      end.not_to change { subject.display }
+      expect(subject.coin_return).to contain_exactly(:penny, :peso)
+    end
   end
 end
